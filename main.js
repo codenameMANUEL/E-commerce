af451db5ecd5ad4dadd393b37990690cb8bf3015
Emmanuel Nkaja <96911514+codenameMANUEL@users.noreply.github.com>
2023-08-23 16:49:52 +0100

    23 aug

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let cart = document.querySelectorAll('.add_to_cart');
+let arr = [3,5,1,2,3,5,6,6,7,8,1]
+let left = []
+let right = []
+let size = parseInt(arr.length)/2;
 
-for (let i=0; i < cart.length; i++){
-    cart[i].addEventListener('click', ()=>{
-        console.log("added to cart")
-    })
+for(let i=0;i<arr.length;i++){
+    if(i < size) left.push(arr[i])
+    if(i >= size) right.push(arr[i])
 }
+console.log(left)
+console.log(right)
