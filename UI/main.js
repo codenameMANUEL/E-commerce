8bc1fcb95ec2765fbdab552e0883466f8a898a2b
Emmanuel Nkaja <96911514+codenameMANUEL@users.noreply.github.com>
2023-09-06 16:03:15 +0100

    send to whatsapp

--- UI/main.js
+++ UI/main.js
@@ -1,129 +1,204 @@
-$(document).ready(function(){
-   
-    function countCartItem(){
-        let count = 0;
-        if(localStorage.getItem("carts")){
-            arr =  JSON.parse(localStorage.getItem('carts'))
-            count = arr.length;
-        }
-        $('#cart-item-count').html(count)
+$(document).ready(function () {
+  function countCartItem() {
+    let count = 0;
+    if (localStorage.getItem("carts")) {
+      arr = JSON.parse(localStorage.getItem("carts"));
+      count = arr.length;
     }
-    countCartItem()
-    // add to cart
-    $(".add_to_cart").click(function(){
+    $("#cart-item-count").html(count);
+  }
+  countCartItem();
 
-        let arr = []
-        let id = $(this).attr('data-attr')
-        let qty = 1
-        let price = $(this).prev().find('.new_price').text()
-        let img = $(this).parent().find('.container img').attr('src')
-       
-        if(id){
-            if(localStorage.getItem("carts")){
-                arr =  JSON.parse(localStorage.getItem('carts'))
-                let item = arr.find(x=>x.id == id );
-                if(item){
-                    item.qty++;
-                }else{
-                    arr.push({
-                        id,
-                        qty,
-                        price,
-                        img
-                    })   
-                }
-            
-            }else{
-                
-                arr.push({
-                    id,
-                    qty,
-                    price,
-                    img
-                })
-              
-            }
-            localStorage.setItem('carts',JSON.stringify(arr))
-            countCartItem()
+  // add to cart
+  $(document).on("click",'.add_to_cart',function(){
+    let arr = [];
+    let id = $(this).attr("data-attr");
+    let qty = 1;
+    let price = $(this).prev().find(".new_price").text();
+    let img = $(this).parent().find(".container img").attr("src");
+
+    if (id) {
+      if (localStorage.getItem("carts")) {
+        arr = JSON.parse(localStorage.getItem("carts"));
+        let item = arr.find((x) => x.id == id);
+        if (item) {
+          item.qty++;
+        } else {
+          arr.push({
+            id,
+            qty,
+            price,
+            img,
+          });
         }
-    })
+      } else {
+        arr.push({
+          id,
+          qty,
+          price,
+          img,
+        });
+      }
+      localStorage.setItem("carts", JSON.stringify(arr));
+      countCartItem();
+    }
+  })
 
-    if($('.avaliableList')){
-        let arr = []
-        if(localStorage.getItem("carts")){
-            arr =  JSON.parse(localStorage.getItem('carts'))
+  if ($(".avaliableList")) {
+    let arr = [];
+    if (localStorage.getItem("carts")) {
+      arr = JSON.parse(localStorage.getItem("carts"));
 
-            if(arr.length){
-                $('.unavaliablelist').hide()
-            }
-        }
-        let html ='';
-       
-        for(let i=0;i<arr.length;i++){
-            let a = arr[i];
-            let total = (parseFloat(a.price.replace('₦','').replace(',','')) * parseInt(a.qty)).toLocaleString()
-            html += ` <tr>
+      if (arr.length) {
+        $(".unavaliablelist").hide();
+      }
+    }
+    let html = "";
+
+    for (let i = 0; i < arr.length; i++) {
+      let a = arr[i];
+      let total = (
+        parseFloat(a.price.replace("₦", "").replace(",", "")) * parseInt(a.qty)
+      ).toLocaleString();
+      html += ` <tr>
             <td><img src="${a.img}"></td>
             <td>${a.price}</td>
             <td>
-                <button class="sub-btn">-</button>
                 <span>${a.qty}</span>
-                <button class="add-btn">+</button>
             </td>
             <td>₦${total}</td>
             <td><button class="delete-btn" data-attr=${a.id}>Delete</button></td>
-        </tr>`
+        </tr>`;
+    }
+    $(".tbody").html(html);
+  }
+
+  document.querySelectorAll(".add-btn").forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const qtyElement = e.target.previousElementSibling;
+      let qty = parseInt(qtyElement.innerText);
+      qtyElement.innerText = ++qty;
+
+      // You'd also update the Total Price here based on the price and qty
+    });
+  });
+
+  function fetchProduct() {
+    $.ajax({
+      method:"GET",
+      url: "https://e-commerce-0r6p.onrender.com/products",
+      success: (arr) => {
+        let html = "";
+       
+        for (let i = 0; i < arr.length; i++) {
+          let a = arr[i];
+          let total = (
+            //parseFloat(a.price.replace("₦", "").replace(",", "")) *
+            parseInt(a.qty)
+          ).toLocaleString();
+          html += `<div class="product_list">
+          <div class="container">
+              <img src="https://images.pexels.com/photos/13936049/pexels-photo-13936049.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="">
+                  <div class="top-right" >
+                      <p>sale</p>
+                  </div>
+          </div>
+          <p style="padding-top: 5px; font-size: 20px;" class="product_name">${a.title}</p>
+          <p style="font-size: 14px;" class="product_id">ZFRT98</p>
+          <div class="price_diff">
+              <p class="old_price">&#8358;40,000</p>
+              <p class="new_price">&#8358;${a.price}</p>
+          </div>
+          <button class="add_to_cart" data-attr="${a._id}">ADD TO CART</button>
+      </div>`;
         }
-        $('.tbody').html(html)
+        $(".product_row").html(html);
+      },
+      error: (err) => {
+        console.log(err)
+      },
+    });
+  }
+
+  document.querySelectorAll(".sub-btn").forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const qtyElement = e.target.nextElementSibling;
+      let qty = parseInt(qtyElement.innerText);
+      if (qty > 1) {
+        qtyElement.innerText = --qty;
+      }
+
+      // You'd also update the Total Price here based on the price and qty
+    });
+  });
+
+  // For delete functionality
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+
+  // Function to handle delete button click
+  function handleDeleteClick(event) {
+    let id = event.target.getAttribute("data-attr");
+    let arr = [];
+    if (localStorage.getItem("carts")) {
+      arr = JSON.parse(localStorage.getItem("carts"));
+      arr = arr.filter((x) => x.id != id);
+      localStorage.setItem("carts", JSON.stringify(arr));
+      countCartItem();
     }
 
-    document.querySelectorAll('.add-btn').forEach(button => {
-        button.addEventListener('click', function(e) {
-            const qtyElement = e.target.previousElementSibling;
-            let qty = parseInt(qtyElement.innerText);
-            qtyElement.innerText = ++qty;
-    
-            // You'd also update the Total Price here based on the price and qty
-        });
-    });
-    
-    document.querySelectorAll('.sub-btn').forEach(button => {
-        button.addEventListener('click', function(e) {
-            const qtyElement = e.target.nextElementSibling;
-            let qty = parseInt(qtyElement.innerText);
-            if(qty > 1) {
-                qtyElement.innerText = --qty;
-            }
-    
-            // You'd also update the Total Price here based on the price and qty
-        });
-    });
-    
-    // For delete functionality
-const deleteButtons = document.querySelectorAll('.delete-btn');
-
-// Function to handle delete button click
-function handleDeleteClick(event) {
-    let id = event.target.getAttribute('data-attr')
-    let arr = []
-    if(localStorage.getItem("carts")){
-        arr =  JSON.parse(localStorage.getItem('carts'))
-        arr = arr.filter(x=>x.id != id);
-        localStorage.setItem('carts',JSON.stringify(arr))
-        countCartItem()
-        
-    }
-    
-    let row = event.target.closest('tr'); // Find the closest table row to the clicked button
+    let row = event.target.closest("tr"); // Find the closest table row to the clicked button
     if (row) {
-        row.remove(); // Remove the row from the table
+      row.remove(); // Remove the row from the table
     }
-}
+  }
+  // SEND CART INFO TO WHATSAPP
 
-// Attach click event listeners to all delete buttons
-deleteButtons.forEach(button => {
-    button.addEventListener('click', handleDeleteClick);
+  // <a href=}
+	// target='_blank'
+	// rel='noreferrer'></a>;
+
+
+  // Attach click event listeners to all delete buttons
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", handleDeleteClick);
+  });
+
+  if( $(".product_row")){
+    
+    fetchProduct()
+  }
+
+
+  if($(".checkBTN")){
+    $(".checkBTN").click(function(){
+      let item = localStorage.getItem('carts')
+      if(item){
+        let arr =  JSON.parse(item);
+        let text =  "Hello welcome to "
+        for (let i = 0; i < arr.length; i++) {
+          let a = arr[i];
+          let total = (
+            parseFloat(a.price.replace("₦", "").replace(",", "")) * parseInt(a.qty)
+          ).toLocaleString();
+
+        
+        text +=`
+        
+        ${a.id} x *${a.qty}*  ${total}
+        
+        User Details
+        *Anestin Jude*
+        *abc@gmail.com*
+        *090876545672*
+        ` 
+        }
+
+        location.href=`https://wa.me/+2348121230526?text=${text}`
+
+      }
+    })
+  }
+
+
 });
-
-});
-
